eb637f18b148fe1c008c99d4a4e096093f144dd0
Paresh Jain <paresh.jain@stridelysolutions.com>
2021-09-10 11:27:42 +0530

    Create my first Asp.net core 3.1 project

---
+++ MyFirstCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstCoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { RollNo = 1, Name = "Paresh", Class = "A" });
+            Student student = new Student();
+            student.RollNo = 2;
+            student.Name = "ABC";
+            student.Class = "B";
+            students.Add(student);
+            ViewData["students"] = students;
+            return View();
+        }
+    }
+}
---
+++ MyFirstCoreApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreApp.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+    }
+}
