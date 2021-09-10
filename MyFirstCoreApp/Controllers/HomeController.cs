6076ecd0253532fe0ac8d82c307c8bd0441272ac
Paresh Jain <paresh.jain@stridelysolutions.com>
2021-09-10 19:10:46 +0530

    Student Registration form w/o use of database for single record

--- MyFirstCoreApp/Controllers/HomeController.cs
+++ MyFirstCoreApp/Controllers/HomeController.cs
@@ -1,23 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyFirstCoreApp.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace MyFirstCoreApp.Controllers
 {
     public class HomeController : Controller
     {
+        List<Student> lstStudent =  new List<Student>();
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(Student model)
+        public IActionResult Index(commonModel model)
         {
+            model.student = new StudentList();
+            model.student.studentList = LoadData(model.studentdata);
             return View(model);
+        }
+
+        public List<Student> LoadData(Student model) {
+            Student student = new Student();
+            if (model != null) {
+                student.RollNo = model.RollNo;
+                student.Name = model.Name;
+                student.Class = model.Class;
+                lstStudent.Add(student); 
+            }
+            return lstStudent;
         }
     }
 }
--- MyFirstCoreApp/Models/Student.cs
+++ MyFirstCoreApp/Models/Student.cs
@@ -1,19 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MyFirstCoreApp.Models
 {
+
+    public class commonModel
+    {
+        public Student studentdata { get; set; }
+        public StudentList student { get; set; }
+    }
     public class Student
     {
         public int RollNo { get; set; }
         public string Name { get; set; }
         public string Class { get; set; }
     }
-    public class studentList
-    {
-        public List<Student> Students { get; set; }
+
+    public class StudentList {
+        public List<Student> studentList { get; set; }
     }
-    
+
 }
